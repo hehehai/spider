7b30435db9a7b76cf628614e4aca15063941a74b
guanwei <riverhohai@gmail.com>
2020-03-24 22:13:36 +0800

    csv file convert json

--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
+const csvToJson = require("csv-file-to-json");
 
 const app = express();
 const uploadFolder = "./upload/";
 
+// 测试
 app.get("/hello", (req, res) => {
   res.send("Good");
 });
 
+// 创建文件保存文件夹
 const createFolder = folder => {
   try {
     fs.accessSync(folder);
@@ -42,7 +45,10 @@ app.post("/upload", upload.single("file"), function(req, res) {
   console.log("文件大小：%s", file.size);
   console.log("文件保存路径：%s", file.path);
 
-  res.send({ code: "200", msg: "OK", data: null });
+  // 转换为json数据，忽略表头
+  const dataInJSON = csvToJson({ filePath: file.path, hasHeader: true });
+
+  res.send({ code: "200", msg: "OK", data: dataInJSON });
 });
 
 app.listen(3001);
