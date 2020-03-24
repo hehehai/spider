f47e17d24f928e6d41cb525f16084b926902c319
guanwei <riverhohai@gmail.com>
2020-03-24 22:06:54 +0800

    server file upload

---
+++ src/server/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const uploadFolder = "./upload/";
+
+app.get("/hello", (req, res) => {
+  res.send("Good");
+});
+
+const createFolder = folder => {
+  try {
+    fs.accessSync(folder);
+  } catch (e) {
+    fs.mkdirSync(folder);
+  }
+};
+
+createFolder(uploadFolder);
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, uploadFolder); // 保存的路径，备注：需要自己创建
+  },
+  filename: function(req, file, cb) {
+    console.log(file);
+    // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
+    cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+  }
+});
+
+// 通过 storage 选项来对 上传行为 进行定制化
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("file"), function(req, res) {
+  var file = req.file;
+
+  console.log("文件类型：%s", file.mimetype);
+  console.log("原始文件名：%s", file.originalname);
+  console.log("文件大小：%s", file.size);
+  console.log("文件保存路径：%s", file.path);
+
+  res.send({ code: "200", msg: "OK", data: null });
+});
+
+app.listen(3001);
