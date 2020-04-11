179b1f3cf0c1a382dcb6a5a6446305e23db73a30
guanwei <riverhohai@gmail.com>
2020-04-11 14:05:30 +0800

    statistics

--- src/views/SpiderPlatform.vue
+++ src/views/SpiderPlatform.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="spider-page">
     <div class="header">
-      <div class="menu">
-        <BackHeader :label="currentPlatformLabel" />
+      <BackHeader :label="currentPlatformLabel" />
+      <div class="control">
+        <el-button type="primary" icon="el-icon-upload">上传文档</el-button>
+        <el-button type="success" icon="el-icon-toilet-paper">抓取</el-button>
+        <el-button icon="el-icon-truck">导出数据</el-button>
+      </div>
+    </div>
+    <div class="wrap">
+      <div class="statistics">
+        <div class="statistics-card"></div>
       </div>
     </div>
   </div>
@@ -32,3 +40,23 @@ export default {
   }
 };
 </script>
+
+<style lang="css">
+.header {
+  display: flex;
+  padding: 8px;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #f5f5f5;
+}
+
+.wrap {
+  padding: 8px;
+}
+
+.statistics-card {
+  height: 120px;
+  border-radius: 6px;
+  border: 1px solid #f5f5f5;
+}
+</style>
