68bfc0fe135c00ebd886afb97f0122571f7534ad
guanwei <riverhohai@gmail.com>
2020-04-12 17:06:52 +0800

    build cofig

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        productName: "spider",
+        win: {
+          icon: "build/spider.ico"
+        },
+        mac: {
+          icon: "build/spider.ico"
+        }
+      }
+    }
+  }
+};
