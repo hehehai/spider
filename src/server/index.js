5c0c67e5c79b7b108235f63235e55b1c9cee5e7c
guanwei <riverhohai@gmail.com>
2020-04-13 11:49:07 +0800

    request limit

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const upload = require("./upload");
 const spider = require("./spider");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 upload(app);
 spider(app);
 
